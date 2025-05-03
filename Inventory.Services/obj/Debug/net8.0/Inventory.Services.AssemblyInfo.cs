78577bd914468b7d5a2288edb3f9a01601265b40
nada mostafa <nd.mostafa5@gmail.com>
2025-05-03 23:06:46 +0300

    updaa

--- Inventory.Services/obj/Debug/net8.0/Inventory.Services.AssemblyInfo.cs
+++ Inventory.Services/obj/Debug/net8.0/Inventory.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3d438182c1465ea34c9abb6947e6b46c22761b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
